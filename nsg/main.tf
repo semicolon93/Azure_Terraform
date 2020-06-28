c3bbea519d001c507bf8332aebc76db52fbf1568
Saikat Sinha <semicolon93@gmail.com>
2020-06-28 10:24:07 +0530

    Create main.tf

---
+++ nsg/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "example" {
+  name     = "acceptanceTestResourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
